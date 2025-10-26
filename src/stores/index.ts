a74e9ad428d367a10544b623bbd3124e651179c3
yiming.qin <2904343971@qq.com>
2025-10-26 19:43:14 +0800

    feat(pinia): pinia集成

---
+++ src/stores/index.ts
@@ -0,0 +1,13 @@
+import { createPinia } from "pinia";
+import type { App } from "vue";
+
+// 导出创建 Pinia 实例的函数
+export function setupPinia(app: App) {
+  const pinia = createPinia();
+  // 可以在这里添加 pinia 插件，例如 pinia-plugin-persistedstate
+  // pinia.use(piniaPluginPersistedstate);
+
+  app.use(pinia);
+}
+
+// ... 导出其他 store
