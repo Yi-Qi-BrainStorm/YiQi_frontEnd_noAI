0c2ae91f0eb2941951962a98c76c67874c6506e2
yiming.qin <2904343971@qq.com>
2025-10-26 16:28:30 +0800

    style(tailwind): 配置tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,vue,html}", // src下任意层级的以.js,ts,vue,html后缀结尾的文件
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
