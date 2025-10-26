02fdd10073380e327a684a2de9e30edae25f532f
yiming.qin <2904343971@qq.com>
2025-10-26 19:06:44 +0800

    feat(scss): 集成scss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
+  <div>
+    <p>hello @</p>
+  </div>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
 
-<style scoped></style>
+<style scoped>
+@import "@assets/hello.scss";
+</style>
