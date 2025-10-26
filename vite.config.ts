cbee69863f6c7038b7c985ad6bb727f5f11d0cb1
yiming.qin <2904343971@qq.com>
2025-10-26 18:10:17 +0800

    feat(component): auto-imports
    通过unplugin-auto-import和unplugin-vue-components实现组件与函数hook的自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import tailwindcss from "@tailwindcss/vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    // 1. 自动导入 Vue Hooks, 路由Hooks, Element Plus Hooks等
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+
+    // 2. 自动导入 Vue 组件和 Element Plus 组件
+    Components({
+      resolvers: [ElementPlusResolver()],
+      // 可选：你可以在这里配置要自动导入的自定义组件目录
+      dirs: ["src/components"],
+    }),
+  ],
 });
